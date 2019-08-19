48ad56b9f28b5d3b82cab46158b5d7c89a6094ff
Morgan Nesbitt <captainmorgan@Morgans-iMac.local>
2019-08-18 20:08:58 -0600

    dont output hashes on non container apps

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -45,7 +45,7 @@ const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 // Some apps do not need the benefits of saving a web request, so not inlining the chunk
 // makes for a smoother build process.
 const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== 'false';
-// Is imbedded app
+// Is embedded app
 const isEmbeddedApp = appPackageJson.projectType === 'embeddedApp';
 const isLibrary = appPackageJson.projectType === 'library';
 const isContainerApp = appPackageJson.projectType === 'servableApp';
@@ -187,13 +187,13 @@ module.exports = function(webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].js'
+        ? isContainerApp ? 'static/js/[name].[contenthash:8].js' : 'static/js/[name].js'
         : isEnvDevelopment && 'static/js/bundle.js',
       // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].chunk.js'
+        ? isContainerApp ? 'static/js/[name].[contenthash:8].chunk.js' : 'static/js/[name].chunk.js'
         : isEnvDevelopment && 'static/js/[name].chunk.js',
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // We use "/" in development.
@@ -636,8 +636,8 @@ module.exports = function(webpackEnv) {
         new MiniCssExtractPlugin({
           // Options similar to the same options in webpackOptions.output
           // both options are optional
-          filename: 'static/css/[name].[contenthash:8].css',
-          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+          filename: isContainerApp ? 'static/css/[name].[contenthash:8].css' : 'static/css/[name].css',
+          chunkFilename: isContainerApp ? 'static/css/[name].[contenthash:8].chunk.css' : 'static/css/[name].chunk.css',
         }),
       // Generate a manifest file which contains a mapping of all asset filenames
       // to their corresponding output file so that tools can pick it up without
